d849bdce9fce26812f0d9298f8abd9f1ba09fb32
Jordan McKoy <jordan.bmp.mckoy@gmail.com>
2023-03-23 20:28:52 +0000

    test

--- src/listeners/events/join-thread.ts
+++ src/listeners/events/join-thread.ts
@@ -12,48 +12,50 @@ export class JoinThreadEvent extends Listener {
         if (!interaction.isButton() || interaction.member?.user.bot || interaction.customId !== BUTTON_IDS.JOIN_THREAD) return;
 
         try {
-
-            const settings = {
-				interviewChannel: CONFIG.interviews.channel,
-			};
             
-			const data = await db.selectFrom('interview').select(['interview.thread_id']).where('application_id', '=', interaction.message.id).executeTakeFirst();
+            console.log("Test")
+            
+            // const settings = {
+			// 	interviewChannel: CONFIG.interviews.channel,
+			// };
+            
+			// const data = await db.selectFrom('interview').select(['interview.thread_id']).where('application_id', '=', interaction.message.id).executeTakeFirst();
 
-			if (!data) return interaction.reply({ content: 'Could not find interview in database', ephemeral: true });
+			// if (!data) return interaction.reply({ content: 'Could not find interview in database', ephemeral: true });
 
-			const applicant = await interaction.guild?.members.fetch(data.thread_id);
+			// const applicant = await interaction.guild?.members.fetch(data.thread_id);
 
-			if (!applicant) return interaction.reply({ content: 'Could not find applicant in guild', ephemeral: true });
+			// if (!applicant) return interaction.reply({ content: 'Could not find applicant in guild', ephemeral: true });
 
-            const channel = (await applicant.guild.channels.fetch(settings.interviewChannel)) as TextChannel;
+            // const channel = (await applicant.guild.channels.fetch(settings.interviewChannel)) as TextChannel;
 
-            if (!channel) return interaction.reply({ content: 'Could not find channel in guild', ephemeral: true });
+            // if (!channel) return interaction.reply({ content: 'Could not find channel in guild', ephemeral: true });
 
-            await channel.guild.channels.fetch()
+            // await channel.guild.channels.fetch()
 
-            const thread = await channel.threads.fetch(data.thread_id);
+            // const thread = await channel.threads.fetch(data.thread_id);
 
-            console.log("Hi");
+            // console.log("Hi");
 
-            if (!thread) return interaction.reply({ content: 'Could not find thread in guild', ephemeral: true });
+            // if (!thread) return interaction.reply({ content: 'Could not find thread in guild', ephemeral: true });
 
-            // const threadChannel = thread as ThreadChannel;
+            // // const threadChannel = thread as ThreadChannel;
 
-            console.log("Hit")
+            // console.log("Hit")
 
-            const interactionUser = await interaction.guild!.members.fetch(interaction.user.id).catch((error) => {
-                client.logger.error(error);
-                return interaction.reply({ content: 'Something went wrong while getting user', ephemeral: true });
-            })
-
-			if (!interactionUser) return interaction.reply({ content: 'Could not find admin', ephemeral: true });
-
-            console.log(interactionUser)
-
-            // await threadChannel.members.add(admin).catch((error) => {
+            // const interactionUser = await interaction.guild!.members.fetch(interaction.user.id).catch((error) => {
             //     client.logger.error(error);
-            //     return interaction.reply({ content: 'Something went wrong while adding member', ephemeral: true });
+            //     return interaction.reply({ content: 'Something went wrong while getting user', ephemeral: true });
             // })
+
+			// if (!interactionUser) return interaction.reply({ content: 'Could not find admin', ephemeral: true });
+
+            // console.log(interactionUser)
+
+            // // await threadChannel.members.add(admin).catch((error) => {
+            // //     client.logger.error(error);
+            // //     return interaction.reply({ content: 'Something went wrong while adding member', ephemeral: true });
+            // // })
 
             return
 
