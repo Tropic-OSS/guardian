47c2611d648f94e5e262099a96a55e43d8ba7af1
TypicalTropic <jordan.bmp.mckoy@gmail.com>
2023-04-28 13:51:07 -0500

    Updated Deny Command To New Schema

--- src/commands/deny.ts
+++ src/commands/deny.ts
@@ -2,8 +2,9 @@ import { ApplyOptions, RequiresGuildContext } from '@sapphire/decorators';
 import { Command } from '@sapphire/framework';
 import { send } from '@sapphire/plugin-editable-commands';
 import { EmbedBuilder, Message } from 'discord.js';
-import { client } from '..';
-import { db } from '../database/db';
+import { prisma } from '../server/db';
+import { INTERVIEW_STATUS } from '../lib/constants';
+import { logger } from '../lib/logger';
 
 @ApplyOptions<Command.Options>({
 	description: 'Deny Member',
@@ -24,89 +25,68 @@ export class UserCommand extends Command {
 
 	@RequiresGuildContext((message: Message) => send(message, 'This command can only be used in servers'))
 	public async chatInputRun(interaction: Command.ChatInputCommandInteraction) {
-		try {
-			const { value } = interaction.options.get('reason', true);
+		if (interaction.member!.user.bot) return interaction.reply('Bots cannot deny members');
 
-			const application = await this.getApplicant(interaction.channelId);
+		const { value } = interaction.options.get('reason', true);
 
-			if (!value) return interaction.reply({ content: 'Please provide a reason', ephemeral: true });
+		const application = await prisma.application.findUnique({
+			where: {
+				interview_thread_id: interaction.channelId
+			}
+		});
 
-			if (application instanceof Error || !application)
-				return interaction.reply({ content: 'Something went wrong trying to deny member', ephemeral: true });
-
-			const member = await interaction.guild?.members.fetch(application.applicant_id);
-
-			if (!member) return interaction.reply({ content: 'Member not found', ephemeral: true });
-
-			await db
-				.updateTable('interview')
-				.set({ status: 'DENIED' })
-				.where('thread_id', '=', interaction.channelId)
-				.execute()
-				.catch((error) => {
-					client.logger.error(error);
-					return interaction.reply({ content: 'Something went wrong trying to deny member', ephemeral: true });
-				});
-
-			await db
-				.insertInto('application_meta')
-				.values({
-					id: application.id,
-					admin_id: interaction.member!.user.id,
-					response: value.toString()
-				})
-				.execute()
-				.catch((error) => {
-					client.logger.error(error);
-					return interaction.reply({ content: 'Something went wrong trying to deny member', ephemeral: true });
-				});
-
-			const denyEmbed = new EmbedBuilder()
-				.setColor('Red')
-				.setTitle(`Denied`)
-				.setAuthor({
-					name: 'Guardian',
-					iconURL: 'https://cdn.discordapp.com/avatars/1063626648399921170/60021a9282221d831512631d8e82b33d.png'
-				})
-				.addFields({ name: 'Reason', value: `${value}`, inline: true })
-				.setTimestamp();
-
-			await member.send({ embeds: [denyEmbed] }).catch((error) => {
-				client.logger.error(error);
-				return interaction.reply({ content: 'Something went wrong trying to denying member', ephemeral: true });
-			});
-
-			await member.kick(value.toString()).catch((error) => {
-				client.logger.error(error);
-				return interaction.reply({ content: 'Something went wrong trying to denying member', ephemeral: true });
-			});
-
-			const embed = new EmbedBuilder()
-				.setColor('Red')
-				.setTitle(`Applicant Denied`)
-				.setAuthor({
-					name: 'Guardian',
-					iconURL: 'https://cdn.discordapp.com/avatars/1063626648399921170/60021a9282221d831512631d8e82b33d.png'
-				})
-				.addFields({ name: 'Member', value: `${member}`, inline: true }, { name: 'Reason', value: `${value}`, inline: true })
-				.setTimestamp();
-
-			return await interaction.reply({ embeds: [embed] }).catch((error) => {
-				client.logger.error(error);
-				return interaction.reply({ content: 'Something went wrong trying to denying member', ephemeral: true });
-			});
-		} catch (error) {
-			client.logger.error(error);
-			return interaction.reply('Something went wrong');
+		if (!application) {
+			return interaction.reply({ content: 'Something went wrong trying to deny member, is this an interview thread ?', ephemeral: true });
 		}
-	}
-	private async getApplicant(threadId: string) {
-		const applicant = await db
-			.selectFrom('application')
-			.innerJoin('interview', 'interview.application_id', 'id')
-			.select(['applicant_id', 'id'])
-			.where('thread_id', '=', threadId)
-			.executeTakeFirst();
-		return applicant;
+
+		const member = await interaction.guild?.members.fetch(application.member_id);
+
+		if (!member) return interaction.reply({ content: 'Member not found', ephemeral: true });
+
+		await prisma.application
+			.update({
+				where: {
+					interview_thread_id: interaction.channelId
+				},
+				data: {
+					interview_status: INTERVIEW_STATUS.DENIED
+				}
+			})
+			.catch((error) => {
+				logger.error(error);
+				return interaction.reply({ content: 'Something went wrong trying to deny member', ephemeral: true });
+			});
+
+		const denyEmbed = new EmbedBuilder()
+			.setColor('Red')
+			.setTitle(`Denied`)
+			.setAuthor({
+				name: 'Guardian',
+				iconURL: 'https://cdn.discordapp.com/avatars/1063626648399921170/60021a9282221d831512631d8e82b33d.png'
+			})
+			.addFields({ name: 'Reason', value: `${value}`, inline: true })
+			.setTimestamp();
+
+		await member.send({ embeds: [denyEmbed] }).catch((error) => {
+			logger.error(error);
+			return interaction.reply({ content: 'Something went wrong trying to denying member', ephemeral: true });
+		});
+
+		await member.kick(value!.toString()).catch((error) => {
+			logger.error(error);
+			return interaction.reply({ content: 'Something went wrong trying to denying member', ephemeral: true });
+		});
+
+		const embed = new EmbedBuilder()
+			.setColor('Red')
+			.setTitle(`Applicant Denied`)
+			.setAuthor({
+				name: 'Guardian',
+				iconURL: 'https://cdn.discordapp.com/avatars/1063626648399921170/60021a9282221d831512631d8e82b33d.png'
+			})
+			.addFields({ name: 'Member', value: `${member}`, inline: true }, { name: 'Reason', value: `${value}`, inline: true })
+			.setTimestamp();
+
+		return await interaction.reply({ embeds: [embed] });
 	}
 }
